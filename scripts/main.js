96464d03a6af47f9a372d294191d041aab92669e
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2019-10-07 16:09:03 -0700

    Finished Ch 13 and Fixed No-Console Error

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,20 @@
   "use strict";
   var FORM_SELECTOR = "[data-coffee-order=\"form\"]";
   var CHECKLIST_SELECTOR = "[data-coffee-order=\"checklist\"]";
+  var SERVER_URL = "http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders";
 
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var RemoteDataStore = App.RemoteDataStore;
   var FormHandler = App.FormHandler;
   var CheckList = App.CheckList;
   var Validation = App.Validation;
 
-  var myTruck = new Truck("ncc-1701", new DataStore());
   window.myTruck = myTruck;
   var checkList = new CheckList(CHECKLIST_SELECTOR);
+  var remoteDS =  new RemoteDataStore(SERVER_URL);
+  var myTruck = new Truck("ncc-1701", remoteDS);
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
 
---
+++ scripts/remotedatastore.js
@@ -0,0 +1,43 @@
+(function(window){
+  "use strict";
+
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function RemoteDataStore(url) { //Vonstructor
+    if(!url){
+      throw new Error("No remote URL Supplied");
+    }
+    this.serverUrl = url;
+  }
+
+  RemoteDataStore.prototype.add = function (key,val) {
+    $.post(this.serverUrl, val,function(response){
+      console.log(response);
+    });
+  };
+
+  RemoteDataStore.prototype.getAll = function (cb) { //get all orders
+    $.get(this.serverUrl, function(response){
+      console.log(response);
+      cb(response); //callback
+    });
+  };
+
+  RemoteDataStore.prototype.get = function(key,cb) { //get all orders based on key
+    $.get(this.serverUrl + "/" + key, function(response){
+      console.log(response);
+      cb(response); //callback
+    });
+  };
+
+  RemoteDataStore.prototype.remove = function (key) {
+    $.ajax(this.serverUrl + "/" + key, {
+      type: "DELETE"
+    });
+  };
+
+  App.RemoteDataStore = RemoteDataStore;
+  window.App = App;
+
+}) (window) ;
